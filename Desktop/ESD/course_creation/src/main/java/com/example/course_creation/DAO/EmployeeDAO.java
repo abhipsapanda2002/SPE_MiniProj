3725802be8ab05c4059cd59a4dcda1620da6cfab
abhipsapanda2002 <155606075+abhipsapanda2002@users.noreply.github.com>
2024-02-15 00:38:57 +0530

    Delete Desktop/ESD directory

--- Desktop/ESD/course_creation/src/main/java/com/example/course_creation/DAO/EmployeeDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.course_creation.DAO;
-
-import com.example.course_creation.Entity.employee;
-
-public interface EmployeeDAO {
-     boolean addEmployee(employee emp);
-
-     boolean verifyEmployee(employee emp);
-}
